aaec1cdb4314fcb984b6683217921971db34e712
MuaazH <muaaz.h.is@gmail.com>
2024-01-07 08:44:09 +0100

    Fixed no-taxes script

--- scripts/no-taxes.c
+++ scripts/no-taxes.c
@@ -4,7 +4,7 @@
 #include <tccdefs.h>
 #include <scripthook.h>
 
-void on_city_order_stats(City *pCity, CityStats *pStats) {
+void on_city_population_stats(City *pCity, CityStats *pStats) {
     if (!pCity->settlement)
         return;
     int taxRate = pCity->settlement->taxRate;
@@ -15,8 +15,24 @@ void on_city_order_stats(City *pCity, CityStats *pStats) {
     pStats->population.decline.taxes2 = 0;
     pStats->population.decline.taxes3 = 0;
     pStats->population.decline.taxes4 = 0;
-    pStats->order.positive.taxes = 5;
+}
+
+void on_city_order_stats(City *pCity, CityStats *pStats) {
+    if (!pCity->settlement)
+        return;
+    int taxRate = pCity->settlement->taxRate;
+    if (taxRate)
+        return; // on works when taxes is set to low
+    pStats->order.positive.taxes = 20;
     pStats->order.negative.taxes = 0;
+}
+
+void on_city_income_stats(City *pCity, CityStats *pStats) {
+    if (!pCity->settlement)
+        return;
+    int taxRate = pCity->settlement->taxRate;
+    if (taxRate)
+        return; // on works when taxes is set to low
     pStats->income.taxes = 0;
     pCity->faction->outdatedTreasury = 1; // tell RTW that there is a Windows Update I mean treasury update. wow this is a long comment. In one line. No breaks. Someone will read this crap one day. Email me if you have line-wrap turned off and you read all of this, muaaz.h.is@gmail.com
 }

56758b9ad1ca8e31bea6b1c6f8b753306e7446e6
MuaazH <muaaz.h.is@gmail.com>
2024-01-20 12:25:11 +0100

    Fixed dbg-building-types.c

--- scripts/dbg-building-types.c
+++ scripts/dbg-building-types.c
@@ -11,7 +11,7 @@ void on_init() {
     char buf[128];
     StrBuilder sb = NEW_STRING_BUILDER(buf, sizeof(buf));
 
-    LinkedBuildingTypeList *list = rtw_get_building_types();
+    BuildingTypeList *list = rtw_get_building_types();
     while (list) {
         BuildingType *type = list->array;
         for (int i = 0; i < list->size; ++i, type++) {
